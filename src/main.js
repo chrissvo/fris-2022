eea30ff28dc98c5d932f9f48bc3575a83c55069c
Christian van Ommeren <git@christian.surf>
2022-12-01 20:18:46 +0100

    Setup wedding website

--- src/main.js
+++ src/main.js
@@ -1,4 +1,36 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp, h } from 'vue'
 
-createApp(App).mount('#app')
+const routes = {
+  '/': 'Home',
+  '/program': 'Program',
+  '/location': 'Location',
+  '/rsvp': 'RSVP',
+  '/thanks': 'Thanks',
+  '/404': '404'
+}
+
+const SimpleRouter = {
+  data: () => ({
+    currentRoute: window.location.pathname,
+    query: Object.fromEntries(new URLSearchParams(window.location.search).entries())
+  }),
+
+  computed: {
+    CurrentComponent() {
+      const route = routes[this.currentRoute] || '/'
+      return require(`./routes/${route}.vue`).default
+    }
+  },
+
+  render() {
+    return h(this.CurrentComponent)
+  },
+
+  created () {
+    window.addEventListener('popstate', () => {
+      this.currentRoute = window.location.pathname
+    })
+  }
+}
+
+createApp(SimpleRouter).mount('#app')
